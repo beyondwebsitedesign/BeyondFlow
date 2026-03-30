8064f6b95b3c95d3ef9520b0dae0434a54873c7c
Beyond Website Design <beyondwebsitedesign@gmail.com>
2026-03-29 20:48:21 -0400

    Fix db

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,14 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // ---------------- MONGODB ----------------
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error('MongoDB connection error:', err));
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected!"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 app.use(cors({ origin: '*' }));
 app.use(express.json());
